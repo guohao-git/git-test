2ce8b4d18fa67b42ff8dc71349ae075de998fb96
guohao <2507102666@qq.com>
2021-08-04 17:34:44 +0800

    github commit

--- src/main/java/com/tphy/GitTest.java
+++ src/main/java/com/tphy/GitTest.java
@@ -7,7 +7,7 @@ package com.tphy;
 public class GitTest {
 
     public static void main(String[] args) {
-        System.out.println("hot-fix");
+        System.out.println("hot-fix ");
         System.out.println("master分支提交的代码");
         System.out.println("hot-fix分支提交的代码");
         System.out.println("将代码提交到gitHub上");
